8fe54c7eeac3d1722922635576bd618b8da441b8
Timur <timk01@mail.ru>
2025-07-08 20:15:39 +0300

    Добавил служебный метод (инициализации)-отрисовки человека

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,95 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final List<String> HANGMAN_VIEW = new ArrayList<>();
+    private static final int ERRORS = 6;
+
+    private static List<String> initHangmanStages() {
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+      +   |
+          |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+     /+   |
+          |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+     /+\\  |
+          |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+     /+\\  |
+     /    |
+          |
+    =========
+    """);
+
+        HANGMAN_VIEW.add("""
+      +---+
+      |   |
+      O   |
+     /+\\  |
+     / \\  |
+          |
+    =========
+    """);
+
+        return HANGMAN_VIEW;
     }
+
+    public static void main(String[] args) {
+        initHangmanStages();
+        printHangman(3);
+    }
+
+    static void printHangman(int errors) {
+        System.out.println(HANGMAN_VIEW.get(errors));
+    }
+
 }
